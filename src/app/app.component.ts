aac3ba67d4a801b66433a93f3e4140b96b35c249
Greg Funk <gregory.c.funk@gmail.com>
2023-10-25 13:20:11 -0400

    basic form and some variables, removes boilerplate angular code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ai-unit-test-generator';
+  isLoading = false;
+  error = '';
+  componentCode = '';
+  unitTestCode = '';
+
+  generateUnitTests() {
+    this.isLoading = true;
+    // Perform some async operation here
+    setTimeout(() => {
+      this.unitTestCode = 'This is the generated unit test code';
+      this.isLoading = false;
+    }, 2000);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; // Import the FormsModule
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule], // Add the FormsModule to the imports array
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
