2d7120fe612b0992457979c994ec60d70955f990
Greg Funk <gregory.c.funk@gmail.com>
2023-10-26 13:25:28 -0400

    holy crap copilot wrote unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,49 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms'; // Import the FormsModule
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    declarations: [AppComponent]
-  }));
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [AppComponent],
+      imports: [FormsModule]
+    }).compileComponents();
   });
 
-  it(`should have as title 'ai-unit-test-generator'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('ai-unit-test-generator');
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create the app', () => {
+    expect(component).toBeTruthy();
   });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('.content span')?.textContent).toContain('ai-unit-test-generator app is running!');
+  });
+
+  describe('generateUnitTests', () => {
+    it('should set isLoading to true', () => {
+      component.generateUnitTests();
+      expect(component.isLoading).toBeTrue();
+    });
+
+    it('should set isLoading to false after 2 seconds', async () => {
+      component.generateUnitTests();
+      await new Promise(resolve => setTimeout(resolve, 2000));
+      expect(component.isLoading).toBeFalse();
+    });
+
+    it('should set unitTestCode to the generated unit test code after 2 seconds', async () => {
+      component.generateUnitTests();
+      await new Promise(resolve => setTimeout(resolve, 2000));
+      expect(component.unitTestCode).toEqual('This is the generated unit test code');
+    });
   });
 });
